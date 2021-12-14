40f03dff8124a68ca609bfaae40451ba608bbcd0
cql <1373611035@qq.com>
2021-12-14 14:19:03 +0800

    更新 config #main

---
+++ packages/ice-core/types/index.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BaseModule, BaseIceRedux } from 'icetf';
+
+export declare class Module extends BaseModule {
+}
+
+declare class _Redux extends BaseIceRedux {
+}
+export const RootRedux : _Redux;

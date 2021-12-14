86191cc5097cc80ee1c83605b3fc59335d1cf713
cql <1373611035@qq.com>
2021-12-14 14:52:06 +0800

    更新 test #packages

--- packages/ice-react-test/src/index.js
+++ packages/ice-react-test/src/index.js
@@ -1,1 +1,1 @@
-export { default as Module } from './Module' 
+export { default as Module } from './Module';
---
+++ packages/ice-react-test/types/index.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { BaseModule } from 'icetf';
+
+export declare class Module extends BaseModule {
+}

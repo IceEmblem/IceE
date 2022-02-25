bdb333ef00c1b1cfedd2d21d831b182f34ddb9ad
cql <1373611035@qq.com>
2022-02-25 11:26:24 +0800

    更新icee config逻辑

--- icee_config/module.js
+++ icee_config/module.js
@@ -1,5 +1,5 @@
 const { paths, getPackagePath } = require('./paths');
-const fs = require('fs')
+const fs = require('fs');
 
 // 获取模块列表
 function getModules(module) {
@@ -30,7 +30,7 @@ function buildModuleListFile(startModule) {
 module.exports.buildModuleListFile = buildModuleListFile;
 
 // 监听模块
-function watchModule(module, customizeOutDir, isDev = true) {
+function compileModule(module, customizeOutDir, iswatch, onexit) {
     const { exec } = require('child_process');
 
     const source = `${getPackagePath(module)}/src`;
@@ -39,14 +39,14 @@ function watchModule(module, customizeOutDir, isDev = true) {
     }
 
     let outDir;
-    if(customizeOutDir){
+    if (customizeOutDir) {
         outDir = customizeOutDir(module);
     }
-    else{
+    else {
         outDir = `${getPackagePath(module)}/dist`;
     }
 
-    const cmd = `${paths.rootPath}/node_modules/.bin/babel ${source} ${isDev ? '-w -s' : ''} --out-dir ${outDir} --copy-files --delete-dir-on-start --extensions .js,.jsx,.ts,.tsx`;
+    const cmd = `${paths.rootPath}/node_modules/.bin/babel ${source} ${iswatch ? '-w -s' : ''} --out-dir ${outDir} --copy-files --delete-dir-on-start --extensions .js,.jsx,.ts,.tsx`;
     console.log(`模块 ${module} :`, cmd);
 
     const babel = exec(cmd, {
@@ -63,49 +63,59 @@ function watchModule(module, customizeOutDir, isDev = true) {
 
     babel.on('close', (code) => {
         console.log(`模块 ${module} 已退出，退出码：${code}`);
+        if (onexit) {
+            onexit();
+        }
     });
 }
-module.exports.watchModule = watchModule;
+module.exports.compileModule = compileModule;
 
 // 监听入口模块所依赖的的模块列表
 function watchModules(startModule, customizeOutDir = null) {
     let modules = getModules(startModule);
     modules.forEach(module => {
-        watchModule(module, customizeOutDir);
-    })
+        compileModule(module, customizeOutDir, true);
+    });
 }
 module.exports.watchModules = watchModules;
 
 // 打包入口模块所依赖的的模块列表
-function babelBuildModules(startModule) {
+function compileModules(startModule, onexit) {
     let modules = getModules(startModule);
+    let runingNum = modules.length;
+    let onexit = () => {
+        runingNum--;
+        if(runingNum <= 0 && onexit) {
+            onexit();
+        }
+    }
     modules.forEach(module => {
-        watchModule(module, null, false);
+        compileModule(module, null, false, onexit);
     })
 }
-module.exports.babelBuildModules = babelBuildModules;
+module.exports.compileModules = compileModules;
 
 // 拷贝模块到 ice-rn-start 的 node_modules 目录（快捷键映射总会存在一些问题，这里直接拷贝）
 function copyModules(startModule) {
-    const {copyDir, rmdir} = require('./utiliy');
+    const { copyDir, rmdir } = require('./utiliy');
 
     let modules = getModules(startModule);
 
     modules.forEach((module) => {
         let source = getPackagePath(module);
-    
+
         let dist = getPackagePath(startModule) + `/node_modules/${module}`;
 
-        if(!fs.existsSync(source)){
+        if (!fs.existsSync(source)) {
             return;
         }
-        
-        if(fs.existsSync(dist)){
-            try{
+
+        if (fs.existsSync(dist)) {
+            try {
                 // 有可能dist是一个快捷方式，可以直接删除
                 fs.rmdirSync(dist);
             }
-            catch{
+            catch {
                 // 否则递归删除
                 rmdir(dist)
             }
--- icee_config/start.js
+++ icee_config/start.js
@@ -1,10 +1,17 @@
-const { watchModules, copyModules } = require('./module');
+const { watchModules, copyModules, compileModules, buildModuleListFile } = require('./module');
 const { getPackagePath } = require('./paths');
 const { execCmd } = require('./utiliy');
+const {createModule} = require('./createModule');
 
-// platform: 'web' | 'native'
-function start(startModule) {
-    let hoistDependencies = require(getPackagePath(startModule)).iceeConfig.hoistDependencies;
+// 调试
+function start(startModule, startCmd) {
+    let package = require(`${getPackagePath(startModule)}/package.json`);
+    if(!package.iceeConfig){
+        console.error(`请为项目${startModule}配置iceeConfig字段`);
+        return;
+    }
+
+    let hoistDependencies = require(`${getPackagePath(startModule)}/package.json`).iceeConfig.hoistDependencies;
 
     if (hoistDependencies) {
         copyModules(startModule);
@@ -15,19 +22,34 @@ function start(startModule) {
     else {
         watchModules(startModule)
     }
+
+    execCmd(`cd ${getPackagePath(startModule)} && ${startCmd}`);
 }
+module.exports.start = start;
 
-console.log(process.argv);
+// 创建模块
+module.exports.createModule = createModule;
 
-// 编译依赖项
-let startCmdIndex = process.argv.findIndex(e => e == '-c');
-if(startCmdIndex < 0){
-    console.log('错误：请输入入口项目运行命令');
-    return;
+// 编译入口模块
+function compileStartModule(startModule) {
+    let package = require(`${getPackagePath(startModule)}/package.json`);
+    if(!package.iceeConfig){
+        console.error(`请为项目${startModule}配置iceeConfig字段`);
+        return;
+    }
+
+    let hoistDependencies = require(`${getPackagePath(startModule)}/package.json`).iceeConfig.hoistDependencies;
+
+    if(hoistDependencies){
+        compileModules(startModule, () => {
+            copyModules(startModule);
+        });
+    }
+    else{
+        compileModules(startModule);
+    }
 }
-let startModule = process.argv[process.argv.length - 1];
-start(startModule);
+module.exports.compileStartModule = compileStartModule;
 
-// 运行入口程序
-let startCmd = process.argv[startCmdIndex + 1];
-execCmd(startCmd);
+// 生成模块的 ModuleList 文件
+module.exports.buildModuleListFile = buildModuleListFile;

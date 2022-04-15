a732c36276183a8195577bdf8c36ee51108fb896
cql <1373611035@qq.com>
2022-04-15 14:28:26 +0800

    应用ice-route-dom

--- packages/ice-react-start/src/index.js
+++ packages/ice-react-start/src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { IEApp } from 'icetf';
+import { Router } from 'ice-route-dom';
 
 // 导入当前模块
 import './Module'
 import './index.css';
-
-const Router = ({pages}) => (
-    <BrowserRouter>
-        <Routes>
-            {pages.map(item => (<Route key={item.url} path={item.url} element={<item.component />} />))}
-        </Routes>
-    </BrowserRouter>
-)
 
 class App extends React.Component {
     render() {

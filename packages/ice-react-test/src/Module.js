e29d629150734cb158e4ef4f4989fb3adfd7be77
cql <1373611035@qq.com>
2022-06-07 14:10:28 +0800

    更新包

--- packages/ice-react-test/src/Module.js
+++ packages/ice-react-test/src/Module.js
@@ -2,14 +2,15 @@ import {BaseModule, PageProvider, ModuleFactory, Page} from 'icetf'
 import {Module as CoreModule} from 'ice-core';
 import Test from './Test'
 
-export default class TestModule extends BaseModule
+class Module extends BaseModule
 {
     initialize(){
         PageProvider.register(new Page("Test", "/Test", Test));
     }
 }
 
+const module = new Module();
+export default module;
+
 // 向模块工厂组成本模块，本模块依赖于 CoreModule 模块
-ModuleFactory.register(TestModule, [
-    CoreModule
-]);
+ModuleFactory.register(module, []);
--- packages/ice-rn-test/src/Module.js
+++ packages/ice-rn-test/src/Module.js
@@ -2,7 +2,7 @@ import {PageProvider, Page, BaseModule, ModuleFactory} from 'icetf'
 import {Module as CoreModule} from 'ice-core';
 import Test from './Test'
 
-export default class Module extends BaseModule
+class Module extends BaseModule
 {
     initialize(){
         // 如果想测试这个模块，就将 /Test 该为 / 吧
@@ -10,6 +10,7 @@ export default class Module extends BaseModule
     }
 }
 
-ModuleFactory.register(Module, [
-    CoreModule
-]);
+const module = new Module();
+export default module;
+
+ModuleFactory.register(module, []);

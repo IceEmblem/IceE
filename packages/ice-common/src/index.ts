261d802bd3c4117bc707ef8bc8c125abd3676cd2
cql <1373611035@qq.com>
2023-06-13 17:49:33 +0800

    修复bug

--- packages/ice-common/src/index.ts
+++ packages/ice-common/src/index.ts
@@ -3,7 +3,7 @@ export { default as Tool } from './Tool';
 export { default as BaseApi } from './apis/BaseApi';
 export { default as iceFetch, iceFetchCallBack } from './apis/iceFetch';
 export { default as token } from './apis/token';
-export { default as iceCreateSlick } from './reduxs/iceCreateSlick';
+export { default as iceCreateSlick, createClearReduxDatasAction } from './reduxs/iceCreateSlick';
 export type { IceSlice, IceSliceState } from './reduxs/iceCreateSlick';
 export { default as GroupMenuProvider, MenuProvider } from './menu/MenuProvider';
 export type { MenuWithUrl, Menu } from './menu/MenuProvider';

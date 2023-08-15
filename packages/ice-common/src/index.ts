1e4124ed8f1d456ae4533146eea460fd9a97823f
cql <1373611035@qq.com>
2023-08-15 11:19:50 +0800

    修复bug

--- packages/ice-common/src/index.ts
+++ packages/ice-common/src/index.ts
@@ -1,7 +1,7 @@
 export { default as Storage } from './Storage';
 export { default as Tool } from './Tool';
 export { default as BaseApi } from './apis/BaseApi';
-export { default as iceFetch, iceFetchCallBack } from './apis/iceFetch';
+export { default as iceFetch, iceFetchCallBack, setDomain } from './apis/iceFetch';
 export { default as token } from './apis/token';
 export { default as iceCreateSlick, createClearReduxDatasAction } from './reduxs/iceCreateSlick';
 export type { IceSlice, IceSliceState, IceSlickOptionType } from './reduxs/iceCreateSlick';
